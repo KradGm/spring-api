64690757ef5583d0c8c7ec26b39384c1548bda61
KradGm <enzo.loliveira@hotmail.com>
2023-10-22 18:11:56 -0300

    first commit

---
+++ padroes/src/main/java/com/padroes/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package com.padroes.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+import com.padroes.model.Endereco;
+
+@FeignClient(name = "viacep", url = "http://viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/" )
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
